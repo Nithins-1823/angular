ded37cf292ec9a8dd4444944054ac895a8bbdfbc
Nithin.s <nithins1823@gmail.com>
2025-04-24 12:53:09 +0530

    Added Location tracking Feature

--- src/app/components/dashboard/home/home.component.ts
+++ src/app/components/dashboard/home/home.component.ts
@@ -22,6 +22,26 @@ export class HomeComponent {
     console.log(`Logged in as ${this.user.displayName}`, this.user);
   }
 
+  // SEND LOCATION METHOD
+   sendLocation() :void {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const accu = position.coords.accuracy;
+        console.log(Latitude is ${latitude},\nLongitude is ${longitude}\nAccuracy is ${accu});
+      },
+      (error) => {
+        console.error("Something went wrong ", error.message);
+      }
+    );
+  } else {
+    console.log("Geolocation is not enabled for this browser");
+  }
+}
+
+
   // SIGN OUT METHOD
 
   auth = inject(Auth);
